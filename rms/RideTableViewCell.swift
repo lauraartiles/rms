894ab70a299144d2235ae9f6989f922dc7aa21f3
Laura A <laura.artiles@mlb.com>
2017-04-20 14:14:20 -0400

    table view cell work

---
+++ rms/RideTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RideTableViewCell.swift
+//  rms
+//
+//  Created by Artiles, Laura on 4/20/17.
+//  Copyright © 2017 Laura Artiles. All rights reserved.
+//
+
+import UIKit
+
+class RideTableViewCell: UITableViewCell {
+  @IBOutlet weak var rideTitle: UILabel!
+  @IBOutlet weak var rideDistance: UILabel!
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+
+    // Configure the view for the selected state
+  }
+
+}
--- rms/RidesViewController.swift
+++ rms/RidesViewController.swift
@@ -9,43 +9,44 @@
 import UIKit
 
 class RidesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let cellIdentifier = "RideCellIdentifier"
-    var rides: [String] = []
-    
+  let cellIdentifier = "RideCellIdentifier"
+  var rides: [String] = []
+
   @IBOutlet weak var tableView: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        rides = ["ride1", "ride2", "ride3"]
-        // Do any additional setup after loading the view, typically from a nib.
-      tableView.delegate = self
-      tableView.dataSource = self
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    rides = ["Monday Morning Ride", "Saturday Morning Ride", "Thursday Afternoon Ride"]
+    // Do any additional setup after loading the view, typically from a nib.
+    tableView.delegate = self
+    tableView.dataSource = self
 
-      self.view.addSubview(self.tableView)
-    }
+    self.view.addSubview(self.tableView)
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rides.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =  tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        
-        // Fetch rides
-        let ride = rides[indexPath.row]
-        
-        // configure cell
-        cell.textLabel?.text = ride
-        
-        return cell
-    }
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return rides.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell =  tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RideTableViewCell
+
+    // Fetch rides
+    let ride = rides[indexPath.row]
+
+    // configure cell
+    cell.rideTitle.text = ride
+    cell.rideDistance.text = "30.0 miles"
+
+    return cell
+  }
 }
 
